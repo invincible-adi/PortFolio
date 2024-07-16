6bdbcfd4c41b1db83ff6a1ed20cca4f406b58962
invincible-adi <134470628+invincible-adi@users.noreply.github.com>
2024-07-16 14:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//typing text animation script
+var typed = new Typed(".typing",{
+    strings : ["Web Devloper","Web Designer","Freelancer","Full Stack Developer",""],
+typeSpeed: 50,
+backSpeed: 10,
+loop: true
+});
